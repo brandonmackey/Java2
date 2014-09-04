f814e586ff090d4916d5ae89dbe753ee0297f6e4
Brandon Mackey <brandonmackey@fullsail.edu>
2014-09-03 22:21:42 -0400

    Wk1 setup commit

---
+++ Fundamentals/app/src/main/java/com/java2wk1/bmackey/fundamentals/MainActivity.java
@@ -0,0 +1,41 @@
+// Brandon Mackey
+// Java 2
+// Week 1
+// Term: 1409
+
+package com.java2wk1.bmackey.fundamentals;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
